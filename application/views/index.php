06dafb8059d3a4f1e9d29de3e376c91e7ec8747f
MNiels00 <fcniels_9@hotmail.com>
2016-01-05 13:15:35 -0600

    Update index.php

--- application/views/index.php
+++ application/views/index.php
@@ -48,79 +48,58 @@
 				</a>
 			</figure>
 		</div>
+		
 		<div>
-			<div>
-				<section class="borde">
-					<h3 class="aline">Iglesias afiliadas</h3>
-				</section>
-			</div>
-			<div>
-				<section>
-					<aside class="row">
-						<aside class="col-lg-4">
-							<div class="cuadro">
-								<aside>
-									<h4>Enlace cristiano Banderilla</h4>
-								</aside>
-								<img class="img-responsive" src="<?=base_url('')."assets/img/1enlace.jpg"?>">
-							</div>
-						</aside>
-						<aside class="col-lg-4">
-							<div class="cuadro">
-								<aside>
-									<h4>Semillas de fuego</h4>
-								</aside>
-								<img class="img-responsive" src="<?=base_url('')."assets/img/2semillas.jpg"?>">
-							</div>
-						</aside>
-						<aside class="col-lg-4">
-							<div class="cuadro">
-								<aside>
-									<h4>Casa de Dios</h4>
-								</aside>
-								<img class="img-responsive" src="<?=base_url('')."assets/img/3casa.jpg"?>">
-							</div>
-						</aside>
-					</aside>
-					<aside class="row">
-						<aside class="col-lg-4">
-							<div class="cuadro">
-								<aside>
-									<h4>Vida en abundancia de M&eacute;xico A.C.</h4>
-								</aside>
-								<img class="img-responsive" src="<?=base_url('')."assets/img/4vida.jpg"?>">
-							</div>
-						</aside>
-						<aside class="col-lg-4">
-							<div class="cuadro">
-								<aside>
-									<h4>Vida nueva</h4>
-								</aside>
-								<img class="img-responsive" src="<?=base_url('')."assets/img/5nueva.jpg"?>">
-							</div>
-						</aside>
-						<aside class="col-lg-4">
-							<div class="cuadro">
-								<aside>
-									<h4>LIDS Xalapa</h4>
-								</aside>
-								<img class="img-responsive" src="<?=base_url('')."assets/img/6lids.jpg"?>">
-							</div>
-						</aside>
-					</aside>
-					<aside class="row">
-						<aside class="col-lg-4">
-							<div class="cuadro">
-								<aside>
-									<h4>Iglesia nacional presbiteriana</h4>
-								</aside>
-								<img class="img-responsive" src="<?=base_url('')."assets/img/7inpm.jpg"?>">
-							</div>
-						</aside>
-					</aside>
-				</section>
-			</div>
+          <h1 class="page-header">Dashboard</h1>
+
+          <div class="row placeholders">
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img width="175" height="175" src="<?=base_url('')."assets/img/1enlace.jpg"?>" class="img-fluid" alt="Generic placeholder thumbnail">
+              <h4>Enlace cristiano Banderilla</h4>
+              <span class="text-muted">Conoce más...</span>
+            </div>
+            
+			<div class="col-xs-6 col-sm-3 placeholder">
+              <img src="<?=base_url('')."assets/img/2semillas.jpg"?>" width="175" height="175" class="img-fluid" alt="Generic placeholder thumbnail">
+              <h4>Semillas de fuego</h4>
+              <span class="text-muted">Conoce más...</span>
+            </div>
+            
+			<div class="col-xs-6 col-sm-3 placeholder">
+              <img src="<?=base_url('')."assets/img/3casa.jpg"?>" width="175" height="175" class="img-fluid" alt="Generic placeholder thumbnail">
+              <h4>Casa de Dios</h4>
+              <span class="text-muted">Conoce más...</span>
+            </div>
+			
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img src="<?=base_url('')."assets/img/4vida.jpg"?>" width="175" height="175" class="img-fluid" alt="Generic placeholder thumbnail">
+              <h4>Vida en abundancia de México A.C.</h4>
+              <span class="text-muted">Conoce más...</span>
+		  </div>
+		  
+		  <div class="col-xs-6 col-sm-3 placeholder">
+              <img src="<?=base_url('')."assets/img/5nueva.jpg"?>" width="175" height="175" class="img-fluid" alt="Generic placeholder thumbnail">
+              <h4>Vida Nueva</h4>
+              <span class="text-muted">Conoce más...</span>
+            </div>
+            
+			<div class="col-xs-6 col-sm-3 placeholder">
+              <img src="<?=base_url('')."assets/img/6lids.jpg"?>" width="175" height="175" class="img-fluid" alt="Generic placeholder thumbnail">
+              <h4>LIDS Xalapa</h4>
+              <span class="text-muted">Conoce más...</span>
+            </div>
+            
+			<div class="col-xs-6 col-sm-3 placeholder">
+              <img src="<?=base_url('')."assets/img/7inpm.jpg"?>" width="250" height="175" class="img-fluid" alt="Generic placeholder thumbnail">
+              <h4>Iglesia nacional presviteriana</h4>
+              <span class="text-muted">Conoce más...</span>
+		  </div>
+		  
+		  
+		  
 		</div>
+	</div>
+		
 		<div class="prefoot">
 			<aside class="row">
 				<aside class="col-lg-4">
